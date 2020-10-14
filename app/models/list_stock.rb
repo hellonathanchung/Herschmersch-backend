0d5c96037656352151abf032685ff48e79744945
hellonathanchung <chung.nathanh@gmail.com>
2020-10-14 16:55:05 -0400

    creates list_stock actions to destroy

--- app/models/list_stock.rb
+++ app/models/list_stock.rb
@@ -3,4 +3,5 @@ class ListStock < ApplicationRecord
   belongs_to :stock
 
   has_one :user, through: :list, dependent: :destroy
+  validates :stock_id, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ Stock.destroy_all
 List.destroy_all
 
 
-User.create(username:"n810", password:"123", password_confirmation:"123", imageUrl:"https://media.giphy.com/media/l4Jz3a8jO92crUlWM/giphy.gif")
+User.create(username: 'n810', password: '123', password_confirmation:'123', imageUrl:'https://media.giphy.com/media/l4Jz3a8jO92crUlWM/giphy.gif')
 5.times do
   User.create(
     imageUrl: Faker::Avatar.image,
@@ -33,25 +33,26 @@ end
       user_id: User.all.sample.id
     )
   end
-Post.create(title:"5 stocks I like", content:"I really like a, b, c, d, and e", user_id: User.all.sample.id)
-Post.create(title:"5 stocks I don't like", content:"I really don't like a, b, c, d, and e")
+Post.create(title:'5 stocks I like', content:'I really like a, b, c, d, and e', user_id: User.all.sample.id)
+Post.create(title:'5 stocks I don't like', content:'I really don't like a, b, c, d, and e')
 
-Category.create(name:"Finance")
-Category.create(name:"Technology")
-Category.create(name:"Healthcare")
+Category.create(name:'Finance')
+Category.create(name:'Technology')
+Category.create(name:'Healthcare')
 
-Stock.create(name:"Microsoft", symbol:"MSFT")
-Stock.create(name:"Apple", symbol:"AAPL")
-Stock.create(name:"Square", symbol:"SQ")
+Stock.create(name:'Microsoft', symbol:'MSFT')
+Stock.create(name:'Apple', symbol:'AAPL')
+Stock.create(name:'Square', symbol:'SQ')
 
-List.create!(title:"Tech", user_id: User.all.sample.id, portfolio: true)
-List.create(title:"REITs", user_id: User.all.sample.id)
+List.create!(title:'Tech', user_id: User.all.sample.id, portfolio: true)
+List.create(title:'REITs', user_id: User.all.sample.id)
+List.create(title:'Portfolio', user_id: User.first.id)
 
 #### Join Tables####
 
-ListStock.create(list_id: List.all.sample.id, stock_id:Stock.all.sample.id)
+ListStock.create(list_id: List.last.id, stock_id:Stock.all.sample.id)
 # UserPost.create(user_id: User.all.sample.id, post_id:Post.all.sample.id)
 PostCategory.create(post_id: Post.all.sample.id, category_id:Category.all.sample.id)
 PostStock.create(stock_id: Stock.all.sample.id, post_id:Post.all.sample.id)
 
-puts "seeded"
+puts 'seeded'
